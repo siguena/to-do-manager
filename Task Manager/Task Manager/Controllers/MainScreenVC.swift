2892e4ab6f1932c0deb000d87305338988667af5
Vladimir Milushev <vladimir.milushev@gmail.com>
2019-02-28 21:15:59 +0200

    Initial project setup; Added app icons and  resources

---
+++ Task Manager/Task Manager/Controllers/MainScreenVC.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreenVC.swift
+//  Task Manager
+//
+//  Created by Vladimir Milushev on 28.02.19.
+//  Copyright © 2019 siguena. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
